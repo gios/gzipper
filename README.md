c19f5ae244176df53e9ab7a9e174a3bfa026e496
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2017-10-28 23:03:09 +0300

    bump 1.1.0

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # gzipper
-Simple script for gzipping your js files.
+Simple CLI for gzipping your js files.
+
+## How to use:
+### Install globally the package.
+
+``` npm i gzipper -g ```
+
+or locally to devDependencies
+
+``` npm i gzipper -D ```
+
+### Run script from global scope or from your package.json as a script;
+Globally usage.
+
+``` gzipper <path> args```
+
+Locally usage.
+* add to scripts property in your package.json
+
+```
+  "scripts": {
+    ...
+    "gzipper": "gzipper"
+  }
+```
+
+* use gzipper with your build commands (ex. Angular CLI)
+```
+  "scripts": {
+    "build": "ng build && gzipper './dist'"
+  }
+```
+
+### Args
+* ```--log``` - enable logging
+
+### Roadmap
+- [x] First work version
+- [ ] Unit tests
+- [ ] Additional arguments for CLI
+- [ ] Your porposal for improvement
+
+### Contribution
+I appreciate every contribution, just fork the repository and send the pull request with your changes.
--- index.js
+++ index.js
@@ -3,15 +3,58 @@ const zlib = require('zlib');
 const fs = require('fs');
 const path = require('path');
 const distFolder = process.argv[2];
+const logLevel = process.argv[3];
+let logger = logLevel === '--log' ? loggerInit(true) : loggerInit(false);
 
 if (!distFolder) {
   throw new Error('Path should be present.');
 }
 
+let globalFilesCount = 0;
+let successGlobalFilesCount = 0;
 const outputDir = path.resolve(__dirname, distFolder);
-const filesList = fs.readdirSync(outputDir).filter(file => path.extname(file) === '.js');
-getNext(filesList, outputDir, () => console.log('Done!'));
+compileFolderRecursively(outputDir);
 
+/**
+ * Compile files in folders recursively
+ * @param {string} outputDir
+ * @param {number} fileCounter
+ */
+function compileFolderRecursively(outputDir) {
+  const filesList = fs.readdirSync(outputDir);
+
+  try {
+    filesList.forEach((file) => {
+      const fullFilePath = path.resolve(outputDir, file);
+
+      if (fs.lstatSync(fullFilePath).isFile()
+        && path.extname(fullFilePath) === '.js') {
+        ++globalFilesCount;
+
+        compressFile(file, outputDir, () => {
+          ++successGlobalFilesCount;
+          logger(`File ${file} has been compiled.`);
+
+          if (globalFilesCount === successGlobalFilesCount) {
+            logger(`${globalFilesCount} files have been compiled.`, true);
+          }
+        });
+      } else if (fs.lstatSync(fullFilePath).isDirectory()) {
+        compileFolderRecursively(fullFilePath);
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * File compression func
+ *
+ * @param {string} filename
+ * @param {string} outputDir
+ * @param {any} callback
+ */
 function compressFile(filename, outputDir, callback) {
   let compress = zlib.createGzip();
   let input = fs.createReadStream(path.join(outputDir, filename));
@@ -25,14 +68,12 @@ function compressFile(filename, outputDir, callback) {
   }
 }
 
-function getNext(filesList, outputDir, callback) {
-  if (filesList.length) {
-    const file = filesList.shift();
-    compressFile(file, outputDir, () => {
-      console.log(`File ${file} has been compiled.`);
-      getNext(filesList, outputDir, callback);
-    });
-  } else if (callback) {
-    callback();
-  }
+
+/**
+ * Custom logger
+ * @param {boolean} enable
+ * @return {Function} logger function
+ */
+function loggerInit(enable) {
+  return (message, force) => ((enable || force) && console.log(message));
 }
