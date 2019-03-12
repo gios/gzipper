5da6f65edd29e1e85928734f08c20243b0348dba
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2019-03-12 23:13:10 +0200

    tests (TODO)

--- test/Gzipper.js
+++ test/Gzipper.js
@@ -1,9 +1,32 @@
-const assert = require('assert')
+// const assert = require('assert')
+// const fs = require('fs')
+// const path = require('path')
 
-describe('Gzipper', () => {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
-  })
-})
+// const Gzipper = require('../Gzipper')
+
+// const RESOURCES_PATH = path.resolve(__dirname, './resources')
+
+// describe('Gzipper', () => {
+//   describe('verbose', () => {
+//     beforeEach(() => removeGzippedFiles())
+
+//     it('should print logs to console about every file', () => {
+//       const options = { verbose: true }
+//       new Gzipper(RESOURCES_PATH, null, options).compress()
+//       this.timeout(100)
+//       assert.equal()
+//     })
+//   })
+// })
+
+// function removeGzippedFiles() {
+//   fs.readdir(RESOURCES_PATH, (err, files) => {
+//     if (err) throw err
+
+//     for (const file of files) {
+//       fs.unlink(path.join(RESOURCES_PATH, file), err => {
+//         if (err) throw err
+//       })
+//     }
+//   })
+// }
