cd83e9aaf92115777fbb6bb637ec44b734a75620
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2019-03-17 20:31:46 +0200

    tests

--- test/Gzipper.js
+++ test/Gzipper.js
@@ -1,4 +1,5 @@
 const assert = require('assert')
+const sinon = require('sinon')
 
 const Gzipper = require('../Gzipper')
 const { RESOURCES_PATH, clear } = require('./utils')
@@ -8,11 +9,20 @@ describe('Gzipper', () => {
     beforeEach(async () => await clear())
 
     it('should print logs to console with --verbose', async () => {
+      const verboseRegExp = /File [^\s]+ has been compressed [^\s]+Kb -> [^\s]+Kb./
+      const messageRegExp = /[^\s]+ files have been compressed./
       const options = { verbose: true }
       const gzipper = new Gzipper(RESOURCES_PATH, null, options)
+      const compressEventSpy = sinon.spy(gzipper.compressEvent, 'emit')
+      const loggerInfoSpy = sinon.spy(gzipper.logger, 'info')
       const message = await gzipper.compress()
-      console.log(message)
-      assert.equal(true, true)
+
+      assert.ok(messageRegExp.test(message))
+      assert.strictEqual(compressEventSpy.callCount, 1)
+      assert.strictEqual(loggerInfoSpy.callCount, 6)
+      for (const [arg] of loggerInfoSpy.args) {
+        assert.ok(verboseRegExp.test(arg))
+      }
     })
 
     afterEach(async () => await clear())
