2669d30eb33af477631d9b1361764fd16421bc8d
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2019-10-03 22:04:24 +0300

    tests

--- test/methods/Gzipper.spec.ts
+++ test/methods/Gzipper.spec.ts
@@ -42,22 +42,58 @@ describe('Methods Gzipper', () => {
     });
   });
 
-  // describe('compileFolderRecursively', () => {
-  //   let sinonSandbox: sinon.SinonSandbox;
+  describe('compileFolderRecursively', () => {
+    let sinonSandbox: sinon.SinonSandbox;
 
-  //   beforeEach(() => {
-  //     sinonSandbox = sinon.createSandbox();
-  //   });
+    beforeEach(() => {
+      sinonSandbox = sinon.createSandbox();
+    });
 
-  //   afterEach(() => {
-  //     sinonSandbox.restore();
-  //     sinon.restore();
-  //   });
+    afterEach(() => {
+      sinonSandbox.restore();
+      sinon.restore();
+    });
 
-  //   it("should ignore entity if it's not a file", async () => {
-  //     const gzipper = new Gzipper(COMPRESS_PATH, null);
-  //     sinonSandbox.stub((gzipper as any).nativeFs, 'readdir').resolves(1);
-  //     await (gzipper as any).compileFolderRecursively();
-  //   });
-  // });
+    it("should ignore entity if it's not a file", async () => {
+      const files = ['one', 'two'];
+      const gzipper = new Gzipper(COMPRESS_PATH, null);
+      sinonSandbox.stub((gzipper as any).nativeFs, 'readdir').resolves(files);
+      sinonSandbox
+        .stub((gzipper as any).nativeFs, 'lstat')
+        .resolves({ isFile: () => false, isDirectory: () => false });
+
+      const compressFile = sinon.fake();
+      sinonSandbox.replace(gzipper, 'compressFile' as any, compressFile);
+      await (gzipper as any).compileFolderRecursively(process.cwd());
+      assert.ok(compressFile.notCalled);
+    });
+
+    it('should print time in seconds if operation takes more than 1000ms', async () => {
+      const MESSAGE_VALIDATION = /File one\.js has been compressed \d+\.\d{4}Kb -> \d+\.\d{4}Kb \([1-9]\d*s \d+\.\d+ms\)/;
+      const files = ['one.js', 'two.js'];
+      const gzipper = new Gzipper(COMPRESS_PATH, null);
+      const infoSpy = sinonSandbox.spy((gzipper as any).logger, 'info');
+      sinonSandbox.stub((gzipper as any).nativeFs, 'readdir').resolves(files);
+      sinonSandbox
+        .stub((gzipper as any).nativeFs, 'lstat')
+        .resolves({ isFile: () => true, isDirectory: () => false });
+
+      const compressFileStub = sinonSandbox
+        .stub(gzipper, 'compressFile' as any)
+        .callsFake(async () => {
+          return new Promise((resolve): void => {
+            setTimeout(
+              () => resolve({ beforeSize: 5000, afterSize: 4000 }),
+              1100,
+            );
+          });
+        });
+
+      await (gzipper as any).compileFolderRecursively(process.cwd());
+      assert.ok(compressFileStub.calledTwice);
+      assert.ok(infoSpy.calledTwice);
+      const [message] = infoSpy.args[0];
+      assert.ok(MESSAGE_VALIDATION.test(message));
+    });
+  });
 });
