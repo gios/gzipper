566c86f2e43c566afdd6fa263d138be09f51ea39
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2019-10-12 20:21:16 +0300

    test

--- index.ts
+++ index.ts
@@ -5,98 +5,66 @@ import { Gzipper } from './src/Gzipper';
 import { GlobalOptions } from './src/interfaces';
 const version = require('./package.json').version;
 
-const {
-  GZIPPER_VERBOSE,
-  GZIPPER_EXCLUDE,
-  GZIPPER_INCLUDE,
-  GZIPPER_THRESHOLD,
-  GZIPPER_GZIP_LEVEL,
-  GZIPPER_GZIP_MEMORY_LEVEL,
-  GZIPPER_GZIP_STRATEGY,
-  GZIPPER_BROTLI,
-  GZIPPER_BROTLI_PARAM_MODE,
-  GZIPPER_BROTLI_QUALITY,
-  GZIPPER_BROTLI_SIZE_HINT,
-  GZIPPER_OUTPUT_FILE_FORMAT,
-} = process.env;
-
 export class Index {
   private target: string | undefined;
   private outputPath: string | undefined;
   private options: GlobalOptions | undefined;
-  private argv: string[] = process.argv;
+  private readonly argv: string[] = process.argv;
+  private readonly env: NodeJS.ProcessEnv = process.env;
 
   public getOptions(): this {
     program
       .version(version)
       .usage('[options] <path> [outputPath]')
-      .option(
-        '-v, --verbose',
-        'detailed level of logs',
-        Boolean(GZIPPER_VERBOSE),
-      )
+      .option('-v, --verbose', 'detailed level of logs')
       .option(
         '-e, --exclude <extensions>',
         'exclude file extensions from compression, example: jpeg,jpg...',
-        (value: string) => value.split(',').map(item => `.${item.trim()}`),
-        GZIPPER_EXCLUDE,
+        this.optionToArray.bind(this),
       )
       .option(
         '-i, --include <extensions>',
         'include file extensions for compression, example: js,css,html...',
-        (value: string) => value.split(',').map(item => `.${item.trim()}`),
-        GZIPPER_INCLUDE,
+        this.optionToArray.bind(this),
       )
       .option(
         '-t, --threshold <number>',
         'exclude assets smaller than this byte size. 0 (default)',
         value => parseInt(value),
-        parseInt(GZIPPER_THRESHOLD as string) || 0,
       )
       .option(
         '-gl, --gzip-level <number>',
         'gzip compression level 6 (default), 0 (no compression) - 9 (best compression)',
         value => parseInt(value),
-        parseInt(GZIPPER_GZIP_LEVEL as string),
       )
       .option(
         '-gm, --gzip-memory-level <number>',
         'amount of memory which will be allocated for compression 8 (default), 1 (minimum memory) - 9 (maximum memory)',
         value => parseInt(value),
-        parseInt(GZIPPER_GZIP_MEMORY_LEVEL as string),
       )
       .option(
         '-gs, --gzip-strategy <number>',
         'compression strategy 0 (default), 1 (filtered), 2 (huffman only), 3 (RLE), 4 (fixed)',
         value => parseInt(value),
-        parseInt(GZIPPER_GZIP_STRATEGY as string),
       )
-      .option(
-        '--brotli',
-        'enable brotli compression, Node.js >= v11.7.0',
-        Boolean(GZIPPER_BROTLI),
-      )
+      .option('--brotli', 'enable brotli compression, Node.js >= v11.7.0')
       .option(
         '-bp, --brotli-param-mode <value>',
         'default, text (for UTF-8 text), font (for WOFF 2.0 fonts)',
-        GZIPPER_BROTLI_PARAM_MODE,
       )
       .option(
         '-bq, --brotli-quality <number>',
         'brotli compression quality 11 (default), 0 - 11',
         value => parseInt(value),
-        parseInt(GZIPPER_BROTLI_QUALITY as string),
       )
       .option(
         '-bs, --brotli-size-hint <number>',
         'expected input size 0 (default)',
         value => parseInt(value),
-        parseInt(GZIPPER_BROTLI_SIZE_HINT as string),
       )
       .option(
         '--output-file-format <value>',
         'output file format with default artifacts [filename].[ext].[compressExt]',
-        GZIPPER_OUTPUT_FILE_FORMAT,
       )
       .option('', 'where:')
       .option('', 'filename -> file name')
@@ -112,18 +80,40 @@ export class Index {
 
     const [target, outputPath] = program.args;
     const options: GlobalOptions = {
-      verbose: program.verbose,
-      exclude: program.exclude,
-      include: program.include,
-      threshold: program.threshold,
-      gzipLevel: program.gzipLevel,
-      gzipMemoryLevel: program.gzipMemoryLevel,
-      gzipStrategy: program.gzipStrategy,
-      brotli: program.brotli,
-      brotliParamMode: program.brotliParamMode,
-      brotliQuality: program.brotliQuality,
-      brotliSizeHint: program.brotliSizeHint,
-      outputFileFormat: program.outputFileFormat,
+      verbose: this.env.GZIPPER_VERBOSE
+        ? !!parseInt(this.env.GZIPPER_VERBOSE as string)
+        : program.verbose,
+      exclude:
+        this.optionToArray(this.env.GZIPPER_EXCLUDE as string) ||
+        program.exclude,
+      include:
+        this.optionToArray(this.env.GZIPPER_INCLUDE as string) ||
+        program.include,
+      threshold:
+        parseInt(this.env.GZIPPER_THRESHOLD as string) ||
+        program.threshold ||
+        0,
+      gzipLevel:
+        parseInt(this.env.GZIPPER_GZIP_LEVEL as string) || program.gzipLevel,
+      gzipMemoryLevel:
+        parseInt(this.env.GZIPPER_GZIP_MEMORY_LEVEL as string) ||
+        program.gzipMemoryLevel,
+      gzipStrategy:
+        parseInt(this.env.GZIPPER_GZIP_STRATEGY as string) ||
+        program.gzipStrategy,
+      brotli: this.env.GZIPPER_BROTLI
+        ? !!parseInt(this.env.GZIPPER_BROTLI as string)
+        : program.brotli,
+      brotliParamMode:
+        this.env.GZIPPER_BROTLI_PARAM_MODE || program.brotliParamMode,
+      brotliQuality:
+        parseInt(this.env.GZIPPER_BROTLI_QUALITY as string) ||
+        program.brotliQuality,
+      brotliSizeHint:
+        parseInt(this.env.GZIPPER_BROTLI_SIZE_HINT as string) ||
+        program.brotliSizeHint,
+      outputFileFormat:
+        this.env.GZIPPER_OUTPUT_FILE_FORMAT || program.outputFileFormat,
     };
 
     this.target = target;
@@ -154,6 +144,14 @@ export class Index {
       (this.options as GlobalOptions) || {},
     );
     gzipper.compress().catch(err => console.error(err));
+  }
+
+  private optionToArray(value: string): string[] | string {
+    if (value) {
+      return value.split(',').map(item => `.${item.trim()}`);
+    }
+
+    return value;
   }
 }
 
--- test/cli/index.spec.ts
+++ test/cli/index.spec.ts
@@ -6,6 +6,17 @@ import { Index } from '../../index';
 describe('Index CLI', () => {
   let sinonSandbox: sinon.SinonSandbox;
 
+  function compareValues(value1: unknown, value2: unknown): boolean {
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      return (
+        value1.length === value2.length &&
+        value1.every((value, index) => value === value2[index])
+      );
+    }
+
+    return value1 === value2;
+  }
+
   beforeEach(() => {
     sinonSandbox = sinon.createSandbox();
   });
@@ -67,8 +78,8 @@ describe('Index CLI', () => {
       outputFileFormat: 'test-[filename].[ext].[compressExt]',
     };
 
-    for (const [key, val] of result.options) {
-      assert.ok(response[key] === val);
+    for (const [key, val] of Object.entries(response)) {
+      assert.ok(compareValues(result.options[key], val));
     }
   });
 
@@ -113,8 +124,80 @@ describe('Index CLI', () => {
       outputFileFormat: 'test-[filename]-out.[ext].[compressExt]',
     };
 
-    for (const [key, val] of result.options) {
-      assert.ok(response[key] === val);
+    for (const [key, val] of Object.entries(response)) {
+      assert.ok(compareValues(result.options[key], val));
+    }
+  });
+
+  it('env variables should overwrite options', () => {
+    const envArguments = {
+      GZIPPER_VERBOSE: '0',
+      GZIPPER_EXCLUDE: 'py,c',
+      GZIPPER_INCLUDE: 'r,rs',
+      GZIPPER_THRESHOLD: '800',
+      GZIPPER_GZIP_LEVEL: '2',
+      GZIPPER_GZIP_MEMORY_LEVEL: '2',
+      GZIPPER_GZIP_STRATEGY: '4',
+      GZIPPER_BROTLI: '0',
+      GZIPPER_BROTLI_PARAM_MODE: 'font',
+      GZIPPER_BROTLI_QUALITY: '3',
+      GZIPPER_BROTLI_SIZE_HINT: '10',
+      GZIPPER_OUTPUT_FILE_FORMAT: '[filename]-[hash].[ext].[compressExt]',
+    };
+    const cliArguments = [
+      'node.exe',
+      'index.js',
+      'folder_to_compress',
+      'folder_to_compress_out',
+      '--verbose',
+      '--exclude',
+      'png,jpg,js',
+      '--include',
+      'png,wav',
+      '--threshold',
+      '500',
+      '--gzip-level',
+      '7',
+      '--gzip-memory-level',
+      '1',
+      '--gzip-strategy',
+      '3',
+      '--brotli',
+      '--brotli-param-mode',
+      'text',
+      '--brotli-quality',
+      '5',
+      '--brotli-size-hint',
+      '77',
+      '--output-file-format',
+      'test-[filename].[ext].[compressExt]',
+    ];
+
+    const index = new Index();
+    (index as any).argv = cliArguments;
+    (index as any).env = envArguments;
+    sinonSandbox.stub(index, 'start' as any).returns(null);
+    const result: any = index.getOptions().filterOptions();
+    assert.strictEqual(result.target, 'folder_to_compress');
+    assert.strictEqual(result.outputPath, 'folder_to_compress_out');
+
+    const response: any = {
+      verbose: false,
+      exclude: ['.py', '.c'],
+      include: ['.r', '.rs'],
+      threshold: 800,
+      gzipLevel: 2,
+      gzipMemoryLevel: 2,
+      gzipStrategy: 4,
+      brotli: false,
+      brotliParamMode: 'font',
+      brotliQuality: 3,
+      brotliSizeHint: 10,
+      outputFileFormat: '[filename]-[hash].[ext].[compressExt]',
+    };
+
+    for (const [key, val] of Object.entries(response)) {
+      assert.ok(compareValues(result.options[key], val));
     }
   });
 });
