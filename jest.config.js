f9fae32ebb945c87d22796427e6c219e4493aa51
gios <pblazhchuk@gmail.com>
2021-12-26 18:04:51 +0200

    tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 20000,
+  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 90,
+      statements: 80,
+    },
+  },
 };
