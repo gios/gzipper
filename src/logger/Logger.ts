13496407cb09417736fadefc8c03439875fd014e
VasylLymych <lymych2000@gmail.com>
2022-07-22 13:32:41 +0300

    feature/Added no-color env support

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -1,57 +1,61 @@
-import { filter } from './filter.decorator';
-import { LogLevel } from './LogLevel.enum';
+import {filter} from './filter.decorator';
+import {LogLevel} from './LogLevel.enum';
 
 /**
  * Custom logger.
  */
 export class Logger {
-  static verbose: boolean;
+    static verbose: boolean;
 
-  /**
-   * Set verbose mode.
-   */
-  static setVerboseMode(verbose: boolean): void {
-    Logger.verbose = verbose;
-  }
-
-  /**
-   * Log message.
-   */
-  @filter()
-  static log<T>(message: T, level: LogLevel = LogLevel.DEBUG): void {
-    return Logger.logger(message, level);
-  }
-
-  /**
-   * Colorize messages depends on the level and return a wrapper.
-   */
-  private static logger<T>(message: T, level: LogLevel): void {
-    let colorfulMessage: string;
-    const prefix = 'gzipper';
-
-    switch (level) {
-      case LogLevel.INFO:
-        colorfulMessage = `\x1b[30;46m${prefix}:\x1b[0m\x1b[36m %s\x1b[0m`;
-        break;
-
-      case LogLevel.ERROR:
-        colorfulMessage = `\x1b[30;41m${prefix}:\x1b[0m\x1b[31m %s\x1b[0m`;
-        break;
-
-      case LogLevel.WARNING:
-        colorfulMessage = `\x1b[30;43m${prefix}:\x1b[0m\x1b[33m %s\x1b[0m`;
-        break;
-
-      case LogLevel.SUCCESS:
-        colorfulMessage = `\x1b[30;42m${prefix}:\x1b[0m\x1b[32m %s\x1b[0m`;
-        break;
-
-      case LogLevel.DEBUG:
-      default:
-        colorfulMessage = `${prefix}: %s`;
-        break;
+    /**
+     * Set verbose mode.
+     */
+    static setVerboseMode(verbose: boolean): void {
+        Logger.verbose = verbose;
     }
 
-    console.log(colorfulMessage, message);
-  }
+    /**
+     * Log message.
+     */
+    @filter()
+    static log<T>(message: T, level: LogLevel = LogLevel.DEBUG): void {
+        return Logger.logger(message, level);
+    }
+
+    /**
+     * Colorize messages depends on the level and return a wrapper.
+     */
+    private static logger<T>(message: T, level: LogLevel): void {
+        let colorfulMessage = '';
+        const prefix = 'gzipper';
+        const colorlessMessage = `${prefix}: %s`
+
+        if (!process.env.NO_COLOR) {
+            switch (level) {
+                case LogLevel.INFO:
+                    colorfulMessage = `\x1b[30;46m${prefix}:\x1b[0m\x1b[36m %s\x1b[0m`;
+                    break;
+
+                case LogLevel.ERROR:
+                    colorfulMessage = `\x1b[30;41m${prefix}:\x1b[0m\x1b[31m %s\x1b[0m`;
+                    break;
+
+                case LogLevel.WARNING:
+                    colorfulMessage = `\x1b[30;43m${prefix}:\x1b[0m\x1b[33m %s\x1b[0m`;
+                    break;
+
+                case LogLevel.SUCCESS:
+                    colorfulMessage = `\x1b[30;42m${prefix}:\x1b[0m\x1b[32m %s\x1b[0m`;
+                    break;
+
+                case LogLevel.DEBUG:
+                default:
+                    colorfulMessage = `${prefix}: %s`;
+                    break;
+            }
+        }
+
+
+        console.log(colorfulMessage || colorlessMessage, message);
+    }
 }
